95b42c2ac6bdfec79361959ffe5e609213a0c9e5
Salem <62039916+salemuxo@users.noreply.github.com>
2025-02-06 19:00:08 +0000

    xTable

--- A1/xTable.c
+++ A1/xTable.c
@@ -6,7 +6,75 @@ Lab Instructor's Name: Salwa Abougamila
 Lecture Instructor's Name: Ardy RD
 --------------------------------------*/
 
+#include <stdio.h>
+#include <math.h>
+#include "xTable.h"
+
 int main()
 {
+    double vals[400];
+    int i = 0;
+
+    for (int y = 5; y <= 100; y += 5)
+    {
+        for (int x = 5; x <= 100; x += 5)
+        {
+            // calculate val based on expression n = (x^4)/(y^4) + sqrt(y)
+            double val = (pow(x, 4) / pow(y, 4)) + sqrt(y);
+            vals[i] = val;
+
+            i++;
+        }
+    }
+
+    // first row, print only x values
+    printf("+         ");
+    for (int x = 5; x <= 100; x += 5)
+    {
+        printf("%d", x);
+        nspaces(10 - idigits(x));
+    }
+    printf("\n");
+
+    for (int y = 5; y <= 100; y += 5)
+    {
+        printf("%d", y);
+        nspaces(10 - idigits(y));
+
+        int start = (y / 5 - 1) * 20;
+        for (int n = start; n < start + 20; n++)
+        {
+            printf("%.1lf", vals[n]);
+            nspaces(10 - ddigits(vals[n]));
+        }
+        printf("\n");
+    }
     return 0;
+}
+
+int idigits(int num)
+{
+    if (num >= 1000) return 4;
+    else if (num >= 100) return 3;
+    else if (num >= 10) return 2;
+    else return 1;
+}
+
+int ddigits(double num)
+{
+    if (num >= 99999.95) return 8;
+    else if (num >= 9999.95) return 7;
+    else if (num >= 999.95) return 6;
+    else if (num >= 99.95) return 5;
+    else if (num >= 9.95) return 4;
+    else return 3;
+}
+
+void nspaces(double n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }
+    return;
 }
---
+++ A1/xTable.h
@@ -0,0 +1,40 @@
+/*--------------------------------------
+Student's Name: Salem Stockdale
+Assignment 1
+Lab Section: X03L
+Lab Instructor's Name: Salwa Abougamila
+Lecture Instructor's Name: Ardy RD
+--------------------------------------*/
+
+/*
+Function to count the number of digits in a positive integer under 10,000.
+
+Input: 
+    int num: a positive integer under 10,000.
+
+Output:
+    returns int: the number of digits in the number.
+*/
+int idigits(int num);
+
+/*
+Function to count the number of digits (including the decimal) in a positive double under 1,000,000 assuming one decimal place. 
+
+Input:
+    double num: a positive double under 1,000,000.
+
+Output:
+    returns int: the number of digits (including the decimal) assuming one decimal place.
+*/
+int ddigits(double num);
+
+/*
+Prints the given number of spaces to the console.
+
+Input:
+    double n: the number of spaces to print.
+
+Output:
+    No return value, but prints n spaces to the console.
+*/
+void nspaces(double n);
