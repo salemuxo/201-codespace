0d71eea0af3a0d8a35492ec5456012a3833ec8c4
Salem <62039916+salemuxo@users.noreply.github.com>
2025-02-07 20:53:44 +0000

    squeeze

--- A1/squeeze.c
+++ A1/squeeze.c
@@ -7,13 +7,44 @@ Lecture Instructor's Name: Ardy RD
 --------------------------------------*/
 
 #include <stdio.h>
+#include <ctype.h>
 
 int main() 
 {
-    char next_char = "";
+    char next_char, prev_char;
+    int counter = 0;
+
     while ((next_char = getchar()) != EOF)
-    {
-        printf("%c", next_char);
+    {        
+        // if next_char is same as prev and counter less than 9, increment
+        if (next_char == prev_char && counter < 9)
+        {
+            counter++;
+        }
+        else
+        {
+            // if counter has counted and prev_char is alphabetical, print prev_char and count
+            if (isalpha(prev_char))
+            {
+                if (counter > 1)
+                {
+                    printf("%c%d", prev_char, counter);
+                }
+                else
+                {
+                    printf("%c", prev_char);
+                }
+            }
+            counter = 1;
+        }
+        // if next_char is newline, print newline
+        if (next_char == '\n')
+        {
+            printf("\n");
+        }
+        // store next_char in prev_char
+        prev_char = next_char;
+
     }
     return 0;
 }
